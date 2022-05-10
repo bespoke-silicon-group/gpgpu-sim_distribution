57b337716b0d4f4279eca0af361823e7264edd40
Aditya <akamath1997@gmail.com>
2022-05-09 21:08:44 -0700

    Update microbenchmark scripts and makefile

--- hb-config/scripts/extract_ptr_chase2.sh
+++ hb-config/scripts/extract_ptr_chase2.sh
@@ -5,8 +5,8 @@ printf "Data size\tRegionSize\tTraversals\tTime\n"
 for i in ${files}; do
 	traversals=$(grep "Traversals" $i | grep -oE "[0-9]+");
 	#echo  | grep -oE "[0-9]+"
-	time=$(grep "Time" $i | grep -oE "[0-9]+");
+	time=($(grep "gpu_sim_cycle" $i | grep -oE "[0-9]+"));
 	threads=$(grep "RegionSize" $i | grep -oE "[0-9]+");
 	Dsize=$(grep "Dsize" $i | grep -oE "[0-9]+");
-	printf "%d\t%d\t%d\t%d\n" $Dsize $threads $traversals $time
+	printf "%d\t%d\t%d\t%d\n" $Dsize $threads $traversals $(expr ${time[2]} - ${time[1]})
 done
